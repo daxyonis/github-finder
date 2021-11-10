7aaa961239c6ae9f53e883c8770106e985bc5d28
daxyonis <emaciejko@gmail.com>
2021-11-10 18:50:58 -0500

    Load users with axios; add env.local and spinner

--- src/App.js
+++ src/App.js
@@ -2,14 +2,38 @@ import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import axios from 'axios';
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+  };
+
+  // Simulate a sleep (useful to check your spinner)
+  async stall(stallTime = 3000) {
+    await new Promise((resolve) => setTimeout(resolve, stallTime));
+  }
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    // await for 2 secs
+    await this.stall(1000);
+
+    this.setState({ loading: false, users: res.data });
+  }
+
   render() {
     return (
       <div className='App'>
         <Navbar />
         <div className='container'>
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
