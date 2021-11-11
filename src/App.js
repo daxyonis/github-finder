e2d837a07117850bb09be82582d0d3db9978a38c
daxyonis <emaciejko@gmail.com>
2021-11-10 20:25:15 -0500

    Add Search and Alert components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
 import axios from 'axios';
 
 class App extends Component {
   state = {
     users: [],
     loading: false,
+    alert: null,
   };
 
   // Simulate a sleep (useful to check your spinner)
@@ -23,16 +26,41 @@ class App extends Component {
     );
 
     // await for 2 secs
-    await this.stall(1000);
+    //await this.stall(1000);
 
     this.setState({ loading: false, users: res.data });
   }
+
+  // search Github users
+  searchUsers = async (query) => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${query}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ loading: false, users: res.data.items });
+  };
+
+  clearUsers = () => this.setState({ users: [], loading: false });
+
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+    setTimeout(() => this.setState({ alert: null }), 4000);
+  };
 
   render() {
     return (
       <div className='App'>
         <Navbar />
         <div className='container'>
+          <Alert alert={this.state.alert} />
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={this.state.users.length > 0}
+            setAlert={this.setAlert}
+          />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
